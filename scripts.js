fee2c2bd062c58fb814ebe2ee2dea5a964517f9f
Stenio Moreira <steniomoreira01@gmail.com>
2021-07-22 23:40:34 -0300

    Atribuido transitions formataçao de valores BR

--- scripts.js
+++ scripts.js
@@ -13,3 +13,83 @@ const Modal = {
             .remove('active')
     }
 }
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/07/2021'
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/07/2021'
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/07/2021'
+    },
+    {
+        id: 4,
+        description: 'API',
+        amount: 20000,
+        date: '23/07/2021'
+    },
+]
+
+const Transaction = {
+    income(){},
+    expenses(){},
+    total(){}
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = DOM.innerHTMLTransition(transaction);
+
+        DOM.transactionsContainer.appendChild(tr);
+    },
+    innerHTMLTransition(transaction){
+        const CSSclass = transaction.amount > 0 ? 'income': 'expense';
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover transação">
+            </td>
+        `;
+
+        return html;
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : "";
+
+        value = String(value).replace(/\D/g,"");
+        value = Number(value) / 100;
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
+
+       return signal + value;
+    }
+}
+
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
